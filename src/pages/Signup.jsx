de0dab9030f33b39cb81ad430cea0452b4adfae7
Hridoy-Modak <hridoychandramodak@gmail.com>
2024-07-08 18:19:32 +0600

    update some desiges left

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SignupImage from "../assets/signupimage.png";
 
 const Signup = () => {
+  let [email, setEmail] = useState(" ");
+  let [name, setName] = useState("");
+  let [pass, setPass] = useState("");
+  // let [submit, setsubmit] = useState()f;
+
+  let handlemail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  let handlename = (e) => {
+    setName(e.target.value);
+  };
+  let handlepass = (e) => {
+    setPass(e.target.value);
+    console.log(e.target.value);
+  };
+
+  console.log(pass);
+
+  let handleSubmit = () => {
+    console.log(email, pass);
+  };
+
   return (
     <div>
       <div className="flex  h-screen font-nunito">
@@ -10,9 +33,12 @@ const Signup = () => {
             <h1 className="text-[30px] font-bold text-secondary mb-3 max-w-[435px]">
               Get started with easily register
             </h1>
-            <p className="mb-[30px] text-[20px]">Free register and you can enjoy it</p>
+            <p className="mb-[30px] text-[20px]">
+              Free register and you can enjoy it
+            </p>
             <div className="relative mt-4">
               <input
+                onChange={handlemail}
                 className="pl-8 w-[365px] h-[50px] border-[1px] border-[#11175D] border-opacity-[30%] rounded-lg"
                 type="text"
                 placeholder="Enter you email"
@@ -23,6 +49,7 @@ const Signup = () => {
             </div>
             <div className="relative mt-10">
               <input
+                onChange={handlename}
                 className="pl-8 w-[365px] h-[50px] border-[1px] border-[#11175D] border-opacity-[30%] rounded-lg"
                 type="text"
                 placeholder="Enter you name"
@@ -34,13 +61,28 @@ const Signup = () => {
             <div className="relative mt-10">
               <input
                 className="pl-8 w-[365px] h-[50px] border-[1px] border-[#11175D] border-opacity-[30%] rounded-lg"
-                type="text"
+                type="password"
                 placeholder="Enter you email"
               />
               <p className="absolute top-[-9px] left-5 bg-white px-3 text-[13px]">
-                Email Adress
+                Password
               </p>
             </div>
+            <button
+              onClick={handleSubmit}
+              className="text-5 text-white font-semibold w-[365px] bg-primary py-3 mt-5 rounded-lg"
+            >
+              Sign Up
+            </button>
+            <p className="text-center mt-5 ">
+              Already Have an Account ?{" "}
+              <span
+                className="text-yellow-600 font-bold cursor-pointer
+          "
+              >
+                login
+              </span>
+            </p>
           </div>
         </div>
         <div className=" w-1/2 h-full">
